98f4c93e85c6fcd680f8beea0cd28ba69e0b2fd9
Emiliya Todorova <104060345+EmiliyaTodorova@users.noreply.github.com>
2022-04-20 11:28:08 +0300

    Add files via upload

---
+++ For Loop/2. Numbers N...1.js
@@ -0,0 +1,16 @@
+function numberN1(input){
+    let n = Number(input[0])
+
+    for(i = n; i > 0; i--){
+
+        
+
+        console.log(i)
+    }
+    
+
+
+
+
+}
+numberN1(["2"])
---
+++ For Loop/3. Numbers 1...N with Step 3.js
@@ -0,0 +1,10 @@
+function numberNWithStep3(input){
+    let n = Number(input[0]);
+
+    for(i = 1; i <= n; i += 3){
+        console.log(i)
+    }
+
+
+}
+numberNWithStep3(["10"])
